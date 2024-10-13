37c707b53452a26afa5949473158cb516620a491
Ashish Kumar <ashishkr4935@gmail.com>
2024-10-13 14:47:14 +0530

    added gitignore file and modified schema.ts

--- src/db/schema/schema.ts
+++ src/db/schema/schema.ts
@@ -1,6 +1,16 @@
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: integer(),
+  name: varchar({ length: 255 }).notNull(),
+  age: integer().notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+});
+
+
+
+// import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+export const userTable = pgTable("user", {
+  id: integer(),
   name: varchar({ length: 255 }).notNull(),
   age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
