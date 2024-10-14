b6e81afc2ead04ade9dddf87192ab9bc3332018f
Jugal kishor@DESKTOP-KOFKCI7.(none) <gcaffe.jugal@gmail.com>
2024-10-14 12:08:48 -0700

    add Supplier schema

---
+++ src/db/schema/SupplierSchema.ts
@@ -0,0 +1,19 @@
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+export const registerTable = pgTable("register", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  company_name_or_owns_car: varchar({ length: 255 }).notNull(),
+  owner_name: varchar({length:255}).notNull,
+  office_address: varchar({length:255}).notNull,
+  country: varchar({length:255}).notNull,
+  city: varchar({length:255}).notNull,
+  zipcode: integer().notNull,
+  office_number: integer().notNull,
+  email: varchar({ length: 255 }).notNull().unique(),
+  contact_person: integer().notNull,
+  mobile_number:integer().notNull,
+  tax_no_or_vat_no: integer().notNull,
+  pan_number: integer().notNull,
+  currency: varchar({length:255}).notNull,
+  image: varchar('image', { length: 255 }).notNull(),    // File name of the uploaded file
+  imagepath: text('image_path').notNull(), 
+});
