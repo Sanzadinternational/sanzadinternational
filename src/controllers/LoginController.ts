195c64817f84c39518c21c7e109614ff813fa8ec
Abhinav agarwal <abhinavagarwal@192.168.1.8>
2025-01-11 11:00:46 +0530

    made single api for dashboard

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -16,18 +16,18 @@ const authenticateUser = async (email: string, password: string, userTable: any)
       Email: userTable.Email,
       Password: userTable.Password,
       Company_name: userTable.Company_name,
+      role: userTable.Role
       // Include other relevant fields
     })
     .from(userTable)
     .where(eq(userTable.Email, email));
 
   if (!user) return null;
-
   // Password validation
   const isPasswordValid = await bcrypt.compare(password, user.Password);
   if (!isPasswordValid) return null;
 
-  const accessToken = jwt.sign({ id: user.Id, email: user.Email }, JWT_SECRET, { expiresIn: '15m' });
+  const accessToken = jwt.sign({ id: user.Id, email: user.Email, role: user.role }, JWT_SECRET, { expiresIn: '30m' });
   return { accessToken, user };
 };
 
@@ -60,5 +60,81 @@ export const FindUser = async (req: Request, res: Response, next: NextFunction) 
 
   } catch (error) {
     next(error); // Pass error to global error handler
+  }
+};
+
+export const dashboard = async (req: Request, res: Response, next: NextFunction) => {
+  const userID = req.body.id;
+  const userRole = req.body.role;
+  
+  if(userRole == 'supplier'){
+  const [user] = await db
+          .select({
+              id:registerTable.id,
+              Company_name:registerTable.Company_name,
+              Owner:registerTable.Owner,
+              Address:registerTable.Address,
+              Country:registerTable.Country, 
+              City:registerTable.City,
+              Zip_code:registerTable.Zip_code,
+              Office_number:registerTable.Office_number,
+              Email:registerTable.Email,
+              Contact_Person:registerTable.Contact_Person,
+              Mobile_number:registerTable.Mobile_number,
+              Gst_Vat_Tax_number:registerTable.Gst_Vat_Tax_number, 
+              PAN_number:registerTable.PAN_number, 
+              Currency:registerTable.Currency,
+              Gst_Tax_Certificate:registerTable.Gst_Tax_Certificate,
+              Role:registerTable.Role
+              // Api_key:registerTable.Api_key,
+              // Is_up:registerTable.Is_up
+          })
+          .from(registerTable)
+          .where(eq(registerTable.id, userID)); 
+  res.status(200).send({
+      success: true,
+      message: "Access granted to protected resource",
+
+      // userId: userID,
+      // user_information: {
+      //     companyName: user.Company_name,
+      // },
+      // role: "supplier",
+
+      userId: req.body.id,
+      Company_name: user.Company_name,
+      Email:user.Email,
+      role: user.Role,
+    });
+  }
+  else if(userRole == 'agent'){
+    const [user] = await db
+            .select({
+                Id:AgentTable.id, 
+                Email: AgentTable.Email, 
+                Company_name:AgentTable.Company_name,
+                Address:AgentTable.Address,
+                Country:AgentTable.Country,
+                City:AgentTable.City,
+                Zip_code:AgentTable.Zip_code,
+                IATA_Code:AgentTable.IATA_Code,
+                Gst_Vat_Tax_number:AgentTable.Gst_Vat_Tax_number,
+                Contact_Person:AgentTable.Contact_Person,
+                Office_number:AgentTable.Office_number,
+                Mobile_number:AgentTable.Mobile_number,
+                Currency:AgentTable.Currency,
+                Role: AgentTable.Role,
+                Gst_Tax_Certificate:AgentTable.Gst_Tax_Certificate
+            })
+            .from(AgentTable)
+            .where(eq(AgentTable.id, userID)); 
+
+            res.status(200).send({
+              success: true,
+              message: "Access granted to protected resource",
+              userId: req.body.id,
+              Email:user.Email,
+              role: user.Role,
+            });
   }
 };
--- src/routes/LoginRoute.ts
+++ src/routes/LoginRoute.ts
@@ -1,7 +1,9 @@
 import express, {Request, Response, NextFunction, Router} from 'express'; 
-import { FindUser } from '../controllers/LoginController';
+import { dashboard, FindUser } from '../controllers/LoginController';
+import authMiddleware from '../middlewares/authMiddleware';
 const router = express.Router(); 
 
 router.post('/login',FindUser);  
+router.get('/dashboard', authMiddleware, dashboard);
 
 export {router as LoginRoute}; 
