b33515594e093df60938faab1c55a56c63bf9674
Jugal kishor@DESKTOP-KOFKCI7.(none) <gcaffe.jugal@gmail.com>
2025-01-17 05:51:46 -0800

    djlsal

--- src/controllers/AdminController.ts
+++ src/controllers/AdminController.ts
@@ -2,6 +2,8 @@ import { Request, Response, NextFunction } from "express";
 import { CreateAdmin } from "../dto/Admin.dto"
 import { AdminTable } from "../db/schema/AdminSchema";
 import { db } from "../db/db";
+const { AgentTable,OneWayTripTable,RoundTripTable } = require('../db/schema/AgentSchema'); 
+import { registerTable } from "../db/schema/SupplierSchema";
 const bcrypt = require('bcrypt'); 
 
 export const CreateAdmins = async (req: Request, res: Response, next: NextFunction) => {
@@ -14,7 +16,7 @@ export const CreateAdmins = async (req: Request, res: Response, next: NextFuncti
         }
 
         // Hash the password before storing it
-        const hashedPassword = await bcrypt.hash(Password, 10);
+        const hashedPassword = await bcrypt.hash(Password, 10); 
 
         // Insert the new admin record
         const result = await db
@@ -40,4 +42,22 @@ export const CreateAdmins = async (req: Request, res: Response, next: NextFuncti
     }
 };
 
+export const AllAgentRecords = async(req:Request,res:Response,next:NextFunction)=>{
+    try{
+         const result = await db.select()
+         .from(AgentTable)
+         return res.status(200).json(result)
+    }catch(error){
+        next(error)
+    }
+}
 
+export const AllGetSuppliers = async(req:Request,res:Response,next:NextFunction)=>{
+    try{
+        const result = await db.select()
+        .from(registerTable) 
+        return res.status(200).json(result)
+    }catch(error){
+        next(error)
+    }
+}
--- src/routes/AdminRoute.ts
+++ src/routes/AdminRoute.ts
@@ -1,8 +1,11 @@
 import express, {Request, Response, NextFunction, Router} from 'express'; 
-import { CreateAdmins } from "../controllers/AdminController";
+import {AllGetSuppliers,AllAgentRecords, CreateAdmins } from "../controllers/AdminController";
+
 const router = express.Router(); 
 
 router.post('/create',CreateAdmins); 
+router.get('/AllAgentRecords',AllAgentRecords);
+router.get('/AllGetSuppliers',AllGetSuppliers); 
 
 export {router as AdminRoute}; 
 
