52cbc0c3a450223c5a2dcc16b372bacd0d03b11a
Jugal kishor@DESKTOP-KOFKCI7.(none) <gcaffe.jugal@gmail.com>
2025-01-22 01:09:43 -0800

    adsfa

--- src/routes/SearchRoute.ts
+++ src/routes/SearchRoute.ts
@@ -1,11 +1,10 @@
 import { dashboard, Supplier_price, TransportNode } from '../controllers/SupplierController';
 import authMiddleware from '../middlewares/authMiddleware';
 import express, {Request, Response, NextFunction, Router} from 'express'; 
-import { fetchFromDatabase, Search } from '../controllers/SearchController'; 
+import {  Search } from '../controllers/SearchController'; 
 
 const router = express.Router(); 
 
 // CreateSearchCar 
-router.get('/fetchFromDatabase',fetchFromDatabase); 
-router.get('/data',Search)
+router.post('/search',Search); 
 export {router as SearchRouter}; 
