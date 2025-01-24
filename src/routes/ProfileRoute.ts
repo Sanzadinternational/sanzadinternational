39783e7b4aa7aa5fe65e599d4ccea9b4856fe350
Jugal kishor@DESKTOP-KOFKCI7.(none) <gcaffe.jugal@gmail.com>
2025-01-24 04:48:05 -0800

    jll

--- src/routes/ProfileRoute.ts
+++ src/routes/ProfileRoute.ts
@@ -1,9 +1,9 @@
 import express, {Request, Response, NextFunction, Router} from 'express'; 
 import { getProfile } from "../controllers/ProfileController"; 
-
+import authMiddleware from '../middlewares/authMiddleware';
 const router = express.Router(); 
  
-router.post('/getProfile', getProfile); 
+router.post('/getProfile',authMiddleware, getProfile); 
 
 export {router as ProfileRoute}; 
 
