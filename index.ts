88f2399882cbd0df4ae1e5e18d4efd2e60432330
Jugal kishor@DESKTOP-KOFKCI7.(none) <gcaffe.jugal@gmail.com>
2025-04-14 11:27:31 +0530

    asdfsaf

--- index.ts
+++ index.ts
@@ -2,77 +2,39 @@ import express  from "express";
 import bodyParser from "body-parser";
 import { SupplierRoute } from "./src/routes";
 import {AgentRoute, LocationRoute} from "./src/routes";
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> develop
 import { AdminRoute } from './src/routes/AdminRoute'
 import { LoginRoute } from "./src/routes/LoginRoute";
 import {SearchRouter} from "./src/routes/SearchRoute";
 import {ProfileRoute} from "./src/routes/ProfileRoute";
-import {BookingRoute} from "./src/routes/BookingRoute";
-<<<<<<< HEAD
->>>>>>> Supplier
-=======
->>>>>>> develop
+import { PaymentRoute } from "./src/routes/PaymentRoute";
 var cors = require('cors')
 // import {SupplierRoute} from './routes/SupplierRoute';
 // const SupplierRoute = require('./src/routes/SupplierRoute');
 // import mongoose from "mongoose";
-<<<<<<< HEAD
-<<<<<<< HEAD
-// var cors = require('cors')
-=======
 // var cors = require('cors') 
->>>>>>> Supplier
-=======
-// var cors = require('cors') 
->>>>>>> develop
 const app = express();
 
+// app.use(cors({
+//     origin:"https://frontend-khtg.vercel.app",
+// }));
 app.use(cors({
-    origin:"http://localhost:3000",
-}));
+    origin: "*", // Allow all origins
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allow all methods
+    allowedHeaders: "Content-Type,Authorization", // Allow specific headers
+  }));
 // app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true})); 
 // app.use("/admin", AdminRoute);
-// app.use("/", (req, res) => {
-//     res.json({
-<<<<<<< HEAD
-<<<<<<< HEAD
-//         "message":"done",
-=======
-//         "message":"done", 
->>>>>>> develop
-//     });
-// });
  
 app.use("/api/V1/supplier", SupplierRoute); 
 app.use("/api/V1", LoginRoute);
 app.use('/api/V1/agent',AgentRoute);
 app.use('/api/V1/location',LocationRoute);
-<<<<<<< HEAD
-=======
-//         "message":"done", 
-//     });
-// });
- 
-app.use("/api/V1/supplier", SupplierRoute); 
-app.use("/api/V1", LoginRoute);
-app.use('/api/V1/agent',AgentRoute);
-app.use('/api/V1/location',LocationRoute);
-=======
->>>>>>> develop
 app.use('/api/V1/admin',AdminRoute) 
 app.use("/api/V1/data",SearchRouter)
 app.use('/api/V1/view',ProfileRoute);
-app.use('/api/V1/Booking',BookingRoute)
-<<<<<<< HEAD
->>>>>>> Supplier
-=======
->>>>>>> develop
+app.use('/api/V1/payment',PaymentRoute);
 // mongoose.connect(MONGOURI).then((result) => {console.log("success")}).catch((error) => {console.error(error)});
 
 
