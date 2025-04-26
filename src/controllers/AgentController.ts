5cf69e6b388d7d67d3095c6c3701af733314e1ec
Sanzadinternational <sanzadinternational5@gmail.com>
2025-04-26 18:01:22 +0530

    Update AgentController.ts

--- src/controllers/AgentController.ts
+++ src/controllers/AgentController.ts
@@ -379,14 +379,14 @@ export const ForgetPassword = async (req: Request, res: Response, next: NextFunc
             const transporter = nodemailer.createTransport({
                 service: 'Gmail', // Replace with your email service provider
                 auth: {
-                    user: 'jugalkishor556455@gmail.com', // Email address from environment variable
-                    pass: 'vhar uhhv gjfy dpes', // Email password from environment variable
+                user: 'sanzadinternational5@gmail.com', // Email address from environment variable
+                pass: 'betf euwp oliy tooq', // Email password from environment variable
                 },
             });
             const resetLink = `http://localhost:8000/api/V1/agent/ResetPassword?token=${Token}`;
             // Send an email with the retrieved data (decrypted password)
             const info = await transporter.sendMail({
-                from: '"Sanzadinternational" <jugalkishor556455@gmail.com>', // Sender address
+                from: '"Sanzadinternational" <sanzadinternational5@gmail.com',>', // Sender address
                 to: `${user[0].Email}`,
                 subject: "Query from Sanzadinternational", // Subject line
                 text: `Details of New Agent Access:\nEmail: ${user[0].Email}`, // Plain text body
